50e45ce5952e5940ed8402c0dbdafb5904cb2695
Derek <derek.baker89@gmail.com>
2019-01-02 16:12:15 +0000

    displays london temperature in browser

--- src/index.js
+++ src/index.js
@@ -1,48 +1,10 @@
-import { Numbers } from './numbers';
+import {Weather} from './weather.js'
+const weather = new Weather()
 
-const numbers = new Numbers();
-const radix = 10;
-let listOfNumbers = [];
-
-function otherWayOfWritingText() {
-  const element = document.createElement('p');
-  element.innerHTML = 'Or I can put it in via a function!';
-  return element;
+async function temperature() {
+  const temp = document.getElementById('temp');
+	temp.innerHTML = await weather.apiCall() + 'ºC';
+	return temp;
 }
 
-function temporarilyClearNumbers() {
-  document.getElementById('list-of-numbers').innerHTML = '';
-}
-
-function displayList() {
-  temporarilyClearNumbers();
-  listOfNumbers.forEach((number) => {
-    const element = document.createElement('li');
-    element.innerHTML = number;
-    document.getElementById('list-of-numbers').appendChild(element);
-  });
-}
-
-function addNumberToList(event) {
-  event.preventDefault();
-  const number = document.getElementById('number').value;
-  listOfNumbers.push(parseInt(number, radix));
-  displayList();
-}
-
-function addOneToEachNumber() {
-  listOfNumbers = numbers.addOneToAll(listOfNumbers);
-  displayList();
-}
-
-
-function clearNumbers() {
-  listOfNumbers = [];
-  displayList();
-}
-
-document.getElementById('other-way-of-writing-text').appendChild(otherWayOfWritingText());
-document.getElementById('add-number').onclick = addNumberToList;
-document.getElementById('add-one-to-each-number').onclick = addOneToEachNumber;
-document.getElementById('temporarily-clear-numbers').onclick = temporarilyClearNumbers;
-document.getElementById('clear-numbers').onclick = clearNumbers;
+temperature();
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/index.js',
@@ -7,4 +8,10 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  node: {
+    fs: 'empty'
+  },
+  plugins: [
+    new Dotenv()
+  ]
 };
