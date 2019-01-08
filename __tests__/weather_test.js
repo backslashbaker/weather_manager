a8484b05dc8b1521b580a879746ca1d7b21a9c5a
Derek <derek.baker89@gmail.com>
2019-01-08 13:44:44 +0000

    updated tests

--- __tests__/weather_test.js
+++ __tests__/weather_test.js
@@ -5,23 +5,23 @@ describe('Weather', () => {
 
   const weather = new Weather();
 
-  describe('Conversion', () => {
-    it('converts from Kelvin to Celcius', () => {
-      expect(weather.convert(273.15)).toEqual('0.0');
-    });
+  // describe('Conversion', () => {
+  //   it('converts from Kelvin to Celcius', () => {
+  //     expect(weather.convert(273.15)).toEqual('0.0');
+  //   });
 
-    it('converts positive temperatures', () => {
-      expect(weather.convert(303.15)).toEqual('30.0');
-    });
+  //   it('converts positive temperatures', () => {
+  //     expect(weather.convert(303.15)).toEqual('30.0');
+  //   });
   
-    it('converts negative temperatures', () => {
-      expect(weather.convert(253.15)).toEqual('-20.0');
-    });
+  //   it('converts negative temperatures', () => {
+  //     expect(weather.convert(253.15)).toEqual('-20.0');
+  //   });
 
     it('can access the weather API for London', async () => {
-       const data = await weather.apiCall();
-      expect(data).toEqual("10.3");
+       const data = await weather.londonWeather();
+      expect(data).toEqual([8.33, "scattered clouds"]);
     });
 
   });
-})
+
