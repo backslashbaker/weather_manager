cb2cc7c5e2b5ef3338da13582d828e9a6a970781
Derek <derek.baker89@gmail.com>
2019-01-08 20:15:32 +0000

    created forecast function to get forecast data

--- __tests__/weather_test.js
+++ __tests__/weather_test.js
@@ -21,9 +21,14 @@ describe('Weather', () => {
   });
 
   it('returns an array of 16 dates as strings in an array',  async () => {
-    console.log(weather.getDates())
+    
    expect(weather.getDates().length).toEqual(16);
  });
+
+  it('returns the temperature and the description of a selected date and time', async () => {
+    const data = await weather.getforecast();
+    expect(typeof data[0]).toEqual('number');
+  });
 
 
 });
