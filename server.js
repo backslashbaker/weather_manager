a3e1569af77a0f581edc602e1df2a4d5f803c8c2
Derek <derek.baker89@gmail.com>
2019-01-15 16:32:04 +0000

    updated server.js with file paths

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(express.static(weather_manager));
+app.use(express.static(__dirname + '/dist'));
 
 // send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(weather_manager, 'index.html'));
+  res.sendFile(path.resolve(__dirname, './index.html'));
 });
 
 app.listen(port);
